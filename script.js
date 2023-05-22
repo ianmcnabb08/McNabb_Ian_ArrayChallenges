7070048eefa9f162a79eb93d626dd6e8167c7566
Ian McNabb <ianmcnabb08@gmail.com>
2023-05-22 17:56:41 -0500

    Array Challenges

---
+++ script.js
@@ -0,0 +1,73 @@
+// 1. Always Hungry
+
+function alwaysHungry(arr) {
+    for (i=0; i<arr.length; i++) {
+        if (arr[i] === "food") {
+        console.log("Yummy")
+        }
+    }
+        if(!arr.includes ("food")) {
+        (console.log("I'm Hungry"))
+    }
+}
+
+// alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// // this should console log "I'm hungry"
+
+
+// 2. High Pass Filter
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i=0; i<=arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+// 3. Better Than Average
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    var avg = (sum/arr.length);
+
+    var count = 0
+    for(let i=0; i<= arr.length; i++) {
+        if (arr[i] > avg) {
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+// 4. Array Reverse
+
+function reverse(arr) {
+    arr.reverse()
+    return arr;
+}
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+// 5. Fibonacci Array
+
+function fibonacciArray(n) {
+    var fibArr = [0, 1];
+    for (var i = 0; i < n-2; i++ ) {
+        fibArr.push(fibArr[i] + (fibArr[i+1]))
+    }
+    return fibArr;
+}
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
